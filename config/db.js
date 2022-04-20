13f5b2af1d48b7c36c8085ab6d2332eda351c881
Hisyam-Dzaki <03hisyam@gmail.com>
2022-04-20 15:37:44 +0700

    signin update & delete reflection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
-const pool = new Pool ({
-    user : '',
+const pool = new Pool({
+    user: 'postgres',
     host: 'localhost',
     database: 'db_reflection',
-    password: ''
+    password: '5432'
 })
 
 module.exports = pool
